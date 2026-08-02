9edbbee7e5376e18d1afbca73f17cca8bca3c589
Luckson-dev <lucksonpremier@gmail.com>
2026-08-02 11:24:31 +0200

    fix depedancy

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-# import os
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import pandas as pd
 import streamlit as st
 from src.prediction import RENTPrediction
